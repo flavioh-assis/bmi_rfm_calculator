160781e8c0f8f4917f71701cb44e79bcf09f1cda
Flavio Assis <flavioh.dev@gmail.com>
2023-12-26 21:51:54 -0300

    Switch navBar to NavigatorBar widget

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -18,31 +18,22 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
+    return NavigationBar(
       backgroundColor: Colors.green.shade600,
-      currentIndex: widget.currentPageIndex,
-      type: BottomNavigationBarType.fixed,
-      showSelectedLabels: true,
-      selectedFontSize: 14,
-      selectedIconTheme: const IconThemeData(size: 24),
-      selectedItemColor: Colors.white,
-      selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-      unselectedFontSize: 12,
-      unselectedIconTheme: const IconThemeData(size: 20),
-      unselectedItemColor: Colors.white.withOpacity(.50),
-      items: const [
-        BottomNavigationBarItem(
+      destinations: const [
+        NavigationDestination(
           label: 'IMC',
           icon: Icon(Icons.scale_outlined),
-          activeIcon: Icon(Icons.scale),
+          selectedIcon: Icon(Icons.scale),
         ),
-        BottomNavigationBarItem(
+        NavigationDestination(
           label: 'IMGR',
           icon: Icon(Icons.pregnant_woman_outlined),
-          activeIcon: Icon(Icons.pregnant_woman),
+          selectedIcon: Icon(Icons.pregnant_woman),
         ),
       ],
-      onTap: (int index) {
+      indicatorColor: Colors.white,
+      onDestinationSelected: (int index) {
         setState(() {
           widget.updatePageIndex(index);
           widget.updateAppBarTitle(index == 0
@@ -50,6 +41,10 @@ class _BottomNavBarState extends State<BottomNavBar> {
               : 'Calcular Índice de Massa Gorda Relativa');
         });
       },
+      overlayColor: const MaterialStatePropertyAll<Color>(
+        Color.fromARGB(255, 226, 226, 226),
+      ),
+      selectedIndex: widget.currentPageIndex,
     );
   }
 }
