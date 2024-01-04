e6b46d19c4beaf22d163353e84b9010e83771881
Flavio Assis <flavioh.dev@gmail.com>
2024-01-04 20:38:36 -0300

    Move app widget to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-import 'app.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculadora de IMC e IMGR',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bmi_rfm_calculator/app.dart';
+import 'package:bmi_rfm_calculator/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
